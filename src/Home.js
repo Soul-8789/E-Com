1e494fb66a13842e35413a760a5f6f5352a71e58
Soul-8789 <102282026+Soul-8789@users.noreply.github.com>
2023-10-25 02:02:38 +0530

    some changes in pages

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,31 @@
 import React from 'react'
+import styled from 'styled-components';
 
 const Home = () => {
   return (
-    <div>
-     <h1>Home</h1>
-    </div>
+    <Wrapper className='test'>
+    Home
+    </Wrapper>
   )
 }
 
+const Wrapper = styled.section`
+  height: 100vh;
+  background-color: ${({ theme }) => theme.colors.bg};
+`;
+
+
 export default Home;
+
+
+// @media (max-width: ${({ theme }) => theme.media.tab}) {
+//     .container {
+//     max-width: 130rem;
+//     padding: 0 3.2rem;
+//   }
+//   }
+
+//    @media (max-width: ${({ theme }) => theme.media.mobile}) {
+//        html {
+//       font-size: 50%;
+//     }
